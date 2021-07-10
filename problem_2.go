ee1fe98eabff7f588e7b67ad2028eea2de36f1cb
Trishul Sathiyamoorthy <trishuls422@gmail.com>
2021-07-09 22:28:35 -0700

    Stuck on problem_2

---
+++ problem_2.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+	Write a program which can compute the factorial of a given numbers.
+	The results should be printed in a comma-separated sequence on a single line.
+	Suppose the following input is supplied to the program:
+*/
+
+// func factorial(n int) int {
+// 	total := 0
+// 	if n == 0 {
+// 		return total
+// 	} else {
+// 		total += (n) * factorial(n-1)
+// 	}
+// 	return total
+// }
+
+// func main() {
+// 	fmt.Println(factorial(8))
+// }
