31f3d6a0e3548813e340abb5bec455c895bbfe1d
Trishul Sathiyamoorthy <trishuls422@gmail.com>
2021-07-16 20:53:21 -0700

    Solved problem 9

--- problem_9.go
+++ problem_9.go
@@ -1,9 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+/*
+	Returns capitalized words in a string with all lowercase words
+*/
+func capitalizeWords(inputText string) string {
+	var capitalizedWords []string
+
+	words := strings.Split(inputText, " ")
+
+	fmt.Println(words)
+
+	for _, word := range words {
+		capitalizedWords = append(capitalizedWords, strings.ToUpper(word))
+	}
+
+	return strings.Join(capitalizedWords, " ")
+}
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("problem 9")
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			break
+		}
 
+		fmt.Println(capitalizeWords(scanner.Text()))
+	}
 }
