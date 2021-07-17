5cfd3b9e2d440539e50af48fd6ad9120c9f3f1d8
Trishul Sathiyamoorthy <trishuls422@gmail.com>
2021-07-16 21:08:44 -0700

    Solved problem 11 and updated progress.txt file

--- problem_11.go
+++ problem_11.go
@@ -4,6 +4,37 @@ import (
 	"fmt"
 )
 
+/*
+	Function that returns true if all digits are even or false if not
+*/
+func hasOnlyEvenDigits(n int) bool {
+
+	flag := true
+
+	for {
+		if n == 0 {
+			break
+		} else {
+			digit := n % 10
+
+			if digit%2 != 0 {
+				flag = false
+			}
+
+			n = n / 10
+		}
+	}
+	return flag
+}
+
+func printNumbersWithEvenDigits(start int, end int) {
+	for i := start; i <= end; i++ {
+		if hasOnlyEvenDigits(i) {
+			fmt.Println(i)
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello!")
+	printNumbersWithEvenDigits(1000, 3000)
 }
